04401b59f9fd7f5ddf278d91449138db97270072
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-06-20 03:08:59 +0500

    task page

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -2,12 +2,16 @@
 import { Form, useActionData, useNavigation } from "react-router-dom";
 import { useEffect, useMemo, useRef, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, updateDoc, doc, collection } from "firebase/firestore";
 import { db } from "../firebase/firebaseConfig";
 import { useFirestore } from "../hooks/useFirestore";
 // Icons & Assets
 import { CiMenuKebab } from "react-icons/ci";
 import emptyBox from "../assets/images/empty-box.png";
+import { IoTrashOutline } from "react-icons/io5";
+import { GrEdit } from "react-icons/gr";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
+import { IoEyeOffOutline } from "react-icons/io5";
 
 // Context and hooks
 import useGlobalContext from "../hooks/useGlobalContext";
@@ -18,7 +22,7 @@ export const action = async ({ request }) => {
   const formData = await request.formData();
   const taskTitle = formData.get("title");
   const taskDate = formData.get("date");
-  const taskTime = formData.get("due-time");
+  const taskTime = formData.get("time");
   const taskBody = formData.get("body");
   const userId = formData.get("userId");
 
@@ -37,11 +41,19 @@ function Tasks() {
   const { deleteDocument, updateDocument } = useFirestore();
   const modalRef = useRef();
   const formRef = useRef();
+  //taskni edit qilish uchun
+  const [editingModal, setEditingModal] = useState(false);
+  const [editingData, setEditingData] = useState(null);
+  const [editingModalLoading, setEditingModalLoading] = useState(false);
+
+  //task filter
+  const [sortCompleted, setSortCompleted] = useState(false);
+  const [sortPending, setSortPending] = useState(false);
 
   const [sendingData, setSendingData] = useState(false);
 
-  const filter = useMemo(() => ["userId", "==", user.uid], [user.uid]);
-  const { data: collectionData } = useAllCollection("Tasks", filter);
+  const filterData = useMemo(() => ["userId", "==", user.uid], [user.uid]);
+  const { data: collectionData } = useAllCollection("Tasks", filterData);
 
   useEffect(() => {
     dispatch({ type: "ADDTASK", payload: collectionData });
@@ -49,6 +61,7 @@ function Tasks() {
 
   // Firebase ga yozish faqat form submit qilingandan so‘ng
   useEffect(() => {
+    setEditingModalLoading(true);
     const addTaskToFirestore = async () => {
       if (result?.taskTitle && !sendingData) {
         setSendingData(true);
@@ -91,6 +104,7 @@ function Tasks() {
           console.error("Firebasega yozishda xatolik:", err);
         } finally {
           setSendingData(false);
+          setEditingModalLoading(false);
         }
       }
     };
@@ -122,60 +136,135 @@ function Tasks() {
   const shortenText = (text, maxLength) => {
     return text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
   };
+
+  //edit function
+  const editFunction = (task) => {
+    setEditingModal(true);
+    setEditingData(task);
+    modalRef.current.showModal();
+  };
+
+  //form submit for editing
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setEditingModalLoading(true);
+
+    const newTitle = e.target.title.value;
+    const newDate = e.target.date.value;
+    const newDueTime = e.target.time.value;
+    const newBody = e.target.body.value;
+
+    const docRef = doc(db, "Tasks", editingData._id);
+
+    await updateDoc(docRef, {
+      taskTitle: newTitle,
+      taskDate: newDate,
+      taskTime: newDueTime,
+      taskBody: newBody,
+    });
+
+    setEditingData(null);
+    setEditingModal(false);
+    setEditingModalLoading(false);
+    formRef.current.reset();
+    modalRef.current.close();
+  };
+
+  // tasklarni filter qilish
+
+  const mapData = useMemo(() => {
+    if (!collectionData) return [];
+
+    if (sortCompleted) {
+      return collectionData.filter((r) => r.status === "Completed");
+    }
+
+    if (sortPending) {
+      return collectionData.filter((r) => r.status === "Pending");
+    }
+
+    return collectionData;
+  }, [collectionData, sortCompleted, sortPending]);
+
   return (
     <div className="w-full min-h-[70vh] py-14 px-10 bg-base-100 border relative">
-      <div className="flex justify-between items-center">
-        <h2 className="text-5xl font-bold text-blue-700">Tasks.</h2>
+      <div className="flex justify-end md:justify-between items-center">
+        <h2 className="text-5xl font-bold text-blue-700 hidden md:block">
+          Tasks.
+        </h2>
         <div className="flex gap-4 items-center">
-          <button className="btn btn-sm">All</button>
+          <button
+            onClick={() => {
+              setSortCompleted(false);
+              setSortPending(false);
+            }}
+            className="btn btn-xs md:btn-sm"
+          >
+            All
+          </button>
           <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-sm m-1">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-xs md:btn-sm m-1"
+            >
               Sort by
             </div>
             <ul className="dropdown-content menu bg-base-100 rounded-box w-52 p-2 shadow z-[1]">
               <li>
-                <a>Pending</a>
+                <button
+                  onClick={() => {
+                    setSortCompleted(false);
+                    setSortPending(true);
+                  }}
+                >
+                  Pending
+                </button>
               </li>
               <li>
-                <a>Completed</a>
-              </li>
-              <li>
-                <a>In Progress</a>
+                <button
+                  onClick={() => {
+                    setSortPending(false);
+                    setSortCompleted(true);
+                  }}
+                >
+                  Completed
+                </button>
               </li>
             </ul>
           </div>
           <button
             onClick={() => modalRef.current?.showModal()}
-            className="btn btn-primary btn-sm"
+            className="btn btn-primary btn-xs md:btn-sm"
           >
             + Add task
           </button>
         </div>
       </div>
-
       <hr className="my-6" />
-
       <div className="mt-10 relative">
         <table className="table">
           <thead>
             <tr>
-              <th className=" max-w-20  lg:font-bold lg:text-xl  ">Complete</th>
+              <th className=" max-w-14 shrink  lg:font-bold lg:text-xl  w-14 ">
+                Complete
+              </th>
               <th className=" lg:text-xl">Title</th>
-              <th className=" lg:text-xl ">Status</th>
-              <th className=" lg:text-xl">Created</th>
-              <th className="lg:text-xl">Due Date</th>
+              <th className=" lg:text-xl hidden md:block">Status</th>
+              <th className=" lg:text-xl hidden md:block">Created</th>
+              <th className="lg:text-xl hidden md:block">Due Date</th>
               <th className="lg:text-xl">Options</th>
             </tr>
           </thead>
           <tbody>
-            {collectionData?.map((task) => (
+            {mapData?.map((task) => (
               <tr
                 key={task.taskId}
                 className={`  ${
-                  task.status === "Completed" ? "bg-gray-50" : ""
+                  task.status === "Completed" ? "bg-base-200" : ""
                 }`}
               >
-                <td>
+                <td className=" ">
                   <div className="flex flex-col gap-1 items-start">
                     <input
                       type="checkbox"
@@ -183,7 +272,7 @@ function Tasks() {
                       onChange={() => changeStatus(task._id, task.status)}
                       className={`checkbox ${
                         task.status === "completed" ? "opasity-50" : ""
-                      } `}
+                      } checkbox-xs md:checkbox-md `}
                     />
                   </div>
                 </td>
@@ -195,25 +284,33 @@ function Tasks() {
                   {shortenText(task.taskTitle, 25)}
                 </td>
                 <td
-                  className={`lg:font-semibold lg:text-lg  ${
+                  className={`hidden md:block lg:font-semibold lg:text-lg  ${
                     task.status === "Completed" ? "opacity-100" : "opacity-50"
                   } `}
                 >
-                  {task.status}
+                  <span
+                    className={`badge ${
+                      task.status === "Completed"
+                        ? "badge-success"
+                        : "badge-warning"
+                    }`}
+                  >
+                    {task.status}
+                  </span>
                 </td>
                 <td
-                  className={`lg:font-semibold lg:text-lg opacity-50   ${
+                  className={`hidden md:block lg:font-semibold lg:text-lg opacity-50 text-[9px] md:text-md   ${
                     task.status === "Completed" && "line-through "
                   }`}
                 >
                   {task.date}
                 </td>
-                <td className="lg:font-semibold lg:text-lg opacity-50">
+                <td className="lg:font-semibold lg:text-lg opacity-50 hidden md:block">
                   {task.taskTime ? (
                     <span
                       className={` ${
                         task.status === "Completed" && "line-through "
-                      }`}
+                      } `}
                     >
                       {task.taskTime}, {task.taskDate}
                     </span>
@@ -221,7 +318,23 @@ function Tasks() {
                     "----"
                   )}
                 </td>
-                <td className="inline-flex justify-center items-center  w-full ">
+                <td className="inline-flex  items-center  w-full  gap-5">
+                  <div>
+                    {task.status === "Completed" ? (
+                      <span>
+                        <IoEyeOffOutline className="w-5 h-5 opacity-50" />
+                      </span>
+                    ) : (
+                      <button
+                        onClick={() => {
+                          document.getElementById("my_modal_3").showModal();
+                          setEditingData(task);
+                        }}
+                      >
+                        <MdOutlineRemoveRedEye className="w-3 h-3 md:w-5 md:h-5 opacity-75" />
+                      </button>
+                    )}
+                  </div>
                   <div className="dropdown dropdown-end">
                     <div tabIndex={0} role="button" className=" m-1 ">
                       <CiMenuKebab className="w-5 h-5 lg:w-7 lg:h-7 " />
@@ -229,16 +342,18 @@ function Tasks() {
 
                     <ul
                       tabIndex={-1}
-                      className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+                      className="dropdown-content menu bg-base-100 rounded-box z-[1] w-32 p-2 shadow"
                     >
                       {task.status !== "Completed" && (
                         <li>
-                          <button>Edit</button>
+                          <button onClick={() => editFunction(task)}>
+                            <GrEdit className="text-warning" /> Edit
+                          </button>
                         </li>
                       )}
                       <li>
                         <button onClick={() => deleteTask(task._id)}>
-                          Delete
+                          <IoTrashOutline className="text-warning" /> Delete
                         </button>
                       </li>
                     </ul>
@@ -249,14 +364,16 @@ function Tasks() {
           </tbody>
         </table>
       </div>
-
       {/* Modal */}
       <dialog ref={modalRef} id="my_modal_2" className="modal">
         <div className="modal-box">
-          <h3 className="text-lg font-bold text-center mb-4">New Task</h3>
+          <h3 className="text-lg font-bold text-center mb-4">
+            {editingModal ? "Edit task" : "New Task"}
+          </h3>
           <Form
+            onSubmit={editingModal ? handleSubmit : undefined}
             method="post"
-            action="."
+            action={editingModal ? undefined : "."}
             ref={formRef}
             className="flex flex-col gap-4"
           >
@@ -265,6 +382,7 @@ function Tasks() {
               <input
                 required
                 name="title"
+                defaultValue={editingModal ? editingData.taskTitle : ""}
                 type="text"
                 className="input input-bordered"
               />
@@ -274,6 +392,7 @@ function Tasks() {
               <input
                 required
                 name="date"
+                defaultValue={editingModal ? editingData.taskDate : ""}
                 type="date"
                 className="input input-bordered"
               />
@@ -281,7 +400,8 @@ function Tasks() {
             <label className="flex flex-col">
               <span>Due Time</span>
               <input
-                name="due-time"
+                name="time"
+                defaultValue={editingModal ? editingData.taskTime : ""}
                 type="time"
                 className="input input-bordered"
               />
@@ -290,17 +410,27 @@ function Tasks() {
               <span>Task Body</span>
               <textarea
                 name="body"
+                defaultValue={editingModal ? editingData.taskBody : ""}
                 className="textarea textarea-bordered"
               ></textarea>
             </label>
             <input type="hidden" name="userId" value={user.uid} />
             <div className="flex gap-4">
-              <button type="submit" className="btn btn-neutral flex-1">
-                Save
+              <button
+                disabled={editingModalLoading}
+                type="submit"
+                className="btn btn-neutral flex-1"
+              >
+                {editingData ? "Save changes" : "Save"}
               </button>
               <button
                 type="button"
-                onClick={() => modalRef.current?.close()}
+                onClick={() => {
+                  setEditingModal(false);
+                  setEditingModalLoading(false);
+                  modalRef.current?.close();
+                  setEditingData(null);
+                }}
                 className="btn btn-outline flex-1"
               >
                 Cancel
@@ -309,13 +439,46 @@ function Tasks() {
           </Form>
         </div>
       </dialog>
+      {/* ======================== task view modal ======================================= */}
+      <dialog id="my_modal_3" className="modal max-w-5xl mx-auto">
+        <div className="modal-box">
+          <h3 className="font-bold text-2xl text-primary">Task</h3>
+          <p className="font-semibold text-xl">
+            <span className="font-bold text-gray-400">Title:</span>{" "}
+            {editingData?.taskTitle}
+          </p>
+          <p className="">
+            <span className="font-bold text-gray-400">Date:</span>{" "}
+            {editingData?.taskDate}
+          </p>
+          <p className="">
+            <span className="font-bold text-gray-400">Date:</span>{" "}
+            {editingData?.taskTime}
+          </p>
+          <p className="">
+            <span className="font-bold text-gray-400">Date:</span>{" "}
+            {editingData?.taskBody}
+          </p>
 
+          <div className="modal-action">
+            {/* if there is a button in form, it will close the modal */}
+            <button
+              onClick={() => {
+                setEditingData(null);
+                document.getElementById("my_modal_3").close();
+              }}
+              className="btn"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </dialog>
       {!collectionData && (
         <div className="absolute inset-0 flex items-center justify-center">
           <span className="loading loading-spinner loading-lg"></span>
         </div>
       )}
-
       {collectionData?.length === 0 && (
         <div className="w-full h-[400px] flex flex-col items-center justify-center opacity-40">
           <img src={emptyBox} className="w-20 mb-2" alt="Empty" />
